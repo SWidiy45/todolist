c13c126971b358965d4e4b4d59fc7ccdcd707dd7
Siska Widiyawati <siskawidiyawati11@gmail.com>
2022-02-06 01:42:05 +0700

    Checklist

--- src/List.js
+++ src/List.js
@@ -1,7 +1,34 @@
 import React from "react";
-import logo_pdf from "./pdf-file.png";
+import checked from "./checked.png";
+import check from "./check.png";
+import { Component } from "react/cjs/react.production.min";
 
 // import { Component } from "react/cjs/react.production.min";
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleStatus: true,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      toggleStatus: !state.toggleStatus,
+    }));
+  }
+
+  render() {
+    return (
+      <button className="checkbox" onClick={this.handleClick}>
+        {this.state.toggleStatus ? <img src={check} /> : <img src={checked} />} <br />
+      </button>
+    );
+  }
+}
 
 function List(props) {
   return (
@@ -9,6 +36,8 @@ function List(props) {
       {props.item.map((item, index) => (
         <div key={index} className="todo-row">
           {item}
+          <Toggle />
+          {/* <input type="checkbox"> {item}</input> */}
         </div>
       ))}
       <button className="download-button">
